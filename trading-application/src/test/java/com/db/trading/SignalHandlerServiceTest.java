77fb25be458f49575f0b1f9aee054e7200c88902
Andrei Pietrușel <andreipietrusel@gmail.com>
2023-05-04 13:55:45 +0300

    Added 1200 signal specs tested with a ParameterizedTest

--- trading-application/src/test/java/com/db/trading/SignalHandlerServiceTest.java
+++ trading-application/src/test/java/com/db/trading/SignalHandlerServiceTest.java
@@ -1,12 +1,30 @@
 package com.db.trading;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InOrder;
 import org.mockito.Mockito;
+import org.mockito.invocation.Invocation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
 public class SignalHandlerServiceTest {
 
@@ -14,8 +32,48 @@ public class SignalHandlerServiceTest {
     Algo algo;
 
     @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    ResourceLoader resourceLoader;
+
+    @Autowired
     private SignalHandlerService signalHandlerService;
 
+    @ParameterizedTest
+    @MethodSource("signalSpecs")
+    void testHandleSignalAllSpecs(int signalId, List<String> actions) throws InvocationTargetException, IllegalAccessException {
+        signalHandlerService.handleSignal(signalId);
+        InOrder orderVerifier = Mockito.inOrder(algo);
+        for (String action : actions) {
+            verifyMethod(orderVerifier, algo, action);
+        }
+        orderVerifier.verify(algo).doAlgo();
+        orderVerifier.verifyNoMoreInteractions();
+    }
+
+    private Stream<Arguments> signalSpecs() throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:signal-specs.json");
+        List<SignalSpec> signalSpecs = objectMapper.readValue(resource.getInputStream(), new TypeReference<>() {
+        });
+        return signalSpecs.stream().map(spec -> Arguments.of(spec.getId(), spec.getActions()));
+    }
+
+    private void verifyMethod(InOrder orderVerifier, Object obj, String methodName) throws InvocationTargetException, IllegalAccessException {
+        Collection<Invocation> invocations = Mockito.mockingDetails(obj).getInvocations();
+        for (Invocation invocation : invocations) {
+            if (invocation.getMethod().getName().equals(methodName)) {
+                Method method = invocation.getMethod();
+                Object[] arguments = invocation.getArguments();
+                method.invoke(orderVerifier.verify(invocation.getMock()), arguments);
+                break;
+            }
+        }
+    }
+
+    /* Older per-signal tests */
+
+    @Disabled
     @Test
     public void testSignal1() {
         signalHandlerService.handleSignal(1);
@@ -28,6 +86,7 @@ public class SignalHandlerServiceTest {
         orderVerifier.verifyNoMoreInteractions();
     }
 
+    @Disabled
     @Test
     public void testSignal2() {
         signalHandlerService.handleSignal(2);
@@ -39,6 +98,7 @@ public class SignalHandlerServiceTest {
         orderVerifier.verifyNoMoreInteractions();
     }
 
+    @Disabled
     @Test
     public void testSignal3() {
         signalHandlerService.handleSignal(3);
